795f996a565e3b6c18532d9a9cafab0dd76efc87
Andrew Poydence <apoydence@pivotal.io>
2018-06-11 06:55:48 -0600

    Improve example script
    The script will watch each process. If
    any process dies, then it will kill all
    the processes so that the container gets
    restarted.

--- examples/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PORT=9999 ./proxy &>> /dev/shm/log &
-HTTP_PROXY=localhost:9999 PORT=10000 ./examples &>> /dev/shm/log &
-HTTP_PROXY=localhost:9999 BACKEND_PORT=10000 ./reverse-proxy &>> /dev/shm/log &
-tail -f /dev/shm/log
